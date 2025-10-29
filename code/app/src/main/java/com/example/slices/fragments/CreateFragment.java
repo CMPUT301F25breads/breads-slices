b2085f8644a2f8fb45f00fb6b072b1f02e990020
bakushiin <braderdely@gmail.com>
2025-10-29 14:27:17 -0600

    Allow for switching buttons

---
+++ code/app/src/main/java/com/example/slices/fragments/CreateFragment.java
@@ -0,0 +1,36 @@
+package com.example.slices.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.slices.databinding.CreateFragmentBinding;
+
+public class CreateFragment extends Fragment {
+    private CreateFragmentBinding binding;
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = CreateFragmentBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
