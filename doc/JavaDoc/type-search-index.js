4eaa069b9b8bc8c7f52475287967fb7bfca721d6
roadkillrh <ryanrotter@gmail.com>
2025-10-30 16:44:41 -0600

    Javadoc done for DBConnector

---
+++ doc/JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example.slices.controllers","l":"DBConnector"},{"p":"com.example.slices.controllers","l":"Logger"},{"p":"com.example.slices.controllers","l":"NotificationManager"}];updateSearchResults();
