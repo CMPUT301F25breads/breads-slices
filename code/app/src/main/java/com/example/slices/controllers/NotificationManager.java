503eb7812e96538f1a45cdd3dd1ac7bcb8be37bd
roadkillrh <ryanrotter@gmail.com>
2025-10-30 17:11:57 -0600

    Small change to NotificationManager

--- code/app/src/main/java/com/example/slices/controllers/NotificationManager.java
+++ code/app/src/main/java/com/example/slices/controllers/NotificationManager.java
@@ -10,7 +10,7 @@ public class NotificationManager {
 
     private static NotificationManager instance ;
     private static int largestId = 0;
-    private DBConnector db = new DBConnector();
+    private static DBConnector db = new DBConnector();
 
 
     private NotificationManager() {
@@ -24,7 +24,7 @@ public class NotificationManager {
         return instance;
     }
 
-    public void sendNotification(String title, String body, int recipientId, int senderId) {
+    public static void sendNotification(String title, String body, int recipientId, int senderId, DBWriteCallback callback) {
         //Check if largestID is greater than 0
         if (largestId > 0) {
             //Increment largestID by 1
@@ -36,7 +36,7 @@ public class NotificationManager {
                 @Override
                 public void onSuccess(int id) {
                     largestId = id;
-                    sendNotification(title, body, recipientId, senderId);
+                    sendNotification(title, body, recipientId, senderId, callback);
                 }
 
                 @Override
@@ -55,11 +55,13 @@ public class NotificationManager {
             public void onSuccess() {
                 //Send a copy to the logger
                 Logger.log(notification);
+                callback.onSuccess();
             }
 
             @Override
             public void onFailure(Exception e) {
                 System.out.println("Failed to write notification: " + e.getMessage());
+                callback.onFailure(e);
             }
 
         });
