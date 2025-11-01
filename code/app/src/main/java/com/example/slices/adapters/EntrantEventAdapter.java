5648849aab9e2dbec7b83a81bbdd274fef335aa9
rprasad22 <rajneel1@ualberta.ca>
2025-11-01 13:32:52 -0600

    EntrantEventAdapter changed to reflect only Card view for the Event_Card.xml. Added To-Dos for Waitlist status for future implementation

--- code/app/src/main/java/com/example/slices/adapters/EntrantEventAdapter.java
+++ code/app/src/main/java/com/example/slices/adapters/EntrantEventAdapter.java
@@ -3,6 +3,7 @@ package com.example.slices.adapters;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.content.res.ColorStateList;
 
@@ -14,6 +15,12 @@ import com.example.slices.Event;
 import com.example.slices.R;
 import com.example.slices.interfaces.EventActions;
 
+import android.view.LayoutInflater;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.bumptech.glide.Glide;
+import com.example.slices.models.Waitlist;
+
 import java.util.List;
 /** EntrantEventAdapter
  * This adpater is for entrants (users) joining or leaving events
@@ -22,16 +29,25 @@ import java.util.List;
  * white text / black text, respectively).
  *
  * @author Raj Prasad
- * Note to Self & Team: Check for any Liskov substitution errors between this and EventAdapter.
- * Note #2: change the code so that it connects directly to DB to check if the event has been joined
- * or not.
+ * Note: later, figure out how to engage the local list/set of waitlisted event IDs to decide
+ * the button state rather than pure Boolean to set fixed states
  */
-public class EntrantEventAdapter extends EventAdapter {
+public class EntrantEventAdapter extends ArrayAdapter<Event> {
     @Nullable
     private EventActions actions;
 
+    /* TODO (Waitlist Status): Local caching of event IDs the user is waitlisted on will go here
+    /* Example:
+    * private final string Set<String> waitlistedEventIds = new HashSet<>();
+    * then call the hosting fragment after it loads the IDs (from DB or local)
+    * public void setWaitlisted Ids(@NonNull Collection<Strings> ids) {
+    * waitlisted EventIds.clear();
+    * waitlistedEventIds.addAll(ids);
+    * notifyDataSetChanged(); }
+     */
+
     public EntrantEventAdapter(Context context, List<Event> events) {
-        super(context, events);
+        super(context, 0, events);
     }
 
     public void setActions(@Nullable EventActions actions) {
@@ -41,30 +57,52 @@ public class EntrantEventAdapter extends EventAdapter {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        View view = super.getView(position, convertView, parent);
+        View view;
 
+        if (convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(R.layout.event_card, parent, false);
+        } else {
+            view = convertView;
+        }
+        // inflate the view first, then grab the event + null guard
         Event event = getItem(position);
         if (event == null) return view;
 
-        // join/leave button color & text color change logic (on the Browse screen, for now)
+        // binding core card views, same as EventAdapter behaviour
+        TextView title = view.findViewById(R.id.event_title);
+        if (title != null) {
+            title.setText(event.getName());
+        }
+        ImageView image = view.findViewById(R.id.image);
+        if (image != null) {
+            Glide.with(getContext()).load(event.getImageUrl()).into(image);
+        }
+
+        // join/leave button (safeguarded if the layout doesnt have it)
         View buttonView = view.findViewById(R.id.btn_event_action);
         if (buttonView instanceof Button) {
             Button actionBtn = (Button) buttonView;
 
-            // set the button text and tag the button as "join"
+            // set the button text and tag the button as "join" AS DEFAULT (TO BE CHANGED:
+            // TODO(Waitlist Status): Before applying default join state, check local IDs list
             actionBtn.setText("Join");
             actionBtn.setBackgroundTintList(ColorStateList.valueOf(
                     ContextCompat.getColor(getContext(), R.color.button_purple)));
             actionBtn.setTextColor(ContextCompat.getColor(getContext(), android.R.color.white));
             actionBtn.setTag("join");
+            // TODO: (Waitlist Status): remove eventID from local IDs list here (if present)
+            // eg., waitlistedEventIds.remove(eventId); notifyDataSetChanged();
 
+            // toggle color/text on click, notify optional callbacks
             actionBtn.setOnClickListener(v -> {
                 boolean isLeave = "leave".equals(actionBtn.getTag());
                 if (isLeave) {
                     // switch to "Join"
                     actionBtn.setText("Join");
+                    // set background color
                     actionBtn.setBackgroundTintList(ColorStateList.valueOf(
                             ContextCompat.getColor(getContext(), R.color.button_purple)));
+                    // set text color
                     actionBtn.setTextColor(ContextCompat.getColor(getContext(), android.R.color.white));
                     actionBtn.setTag("join");
                     if (actions != null) actions.onLeaveClicked(event);
@@ -75,6 +113,8 @@ public class EntrantEventAdapter extends EventAdapter {
                             ContextCompat.getColor(getContext(), android.R.color.white)));
                     actionBtn.setTextColor(ContextCompat.getColor(getContext(), R.color.button_purple));
                     actionBtn.setTag("leave");
+                    // TODO(Waitlist Status): add eventId to local IDs list here too
+                    // e.g., waitlistedEventIds.add(eventId); notifydatasetchanged();
                     if (actions != null) actions.onJoinClicked(event);
                 }
             });
