953c8784ead617c5ade979e72dc79bf6c7963a90
Sasieni T <sasienii@ualberta.ca>
2025-11-05 03:04:06 -0700

    added on click logic for event

--- code/app/src/main/java/com/example/slices/adapters/EventAdapter.java
+++ code/app/src/main/java/com/example/slices/adapters/EventAdapter.java
@@ -1,6 +1,7 @@
 package com.example.slices.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -36,6 +37,28 @@ public class EventAdapter extends ArrayAdapter<Event> {
         title.setText(event.getName());
         ImageView image = view.findViewById(R.id.image);
         Glide.with(this.getContext()).load(event.getImageUrl()).into(image);
+
+        /**
+         * Added click listener for event card
+         */
+        
+        String formattedDate = "";
+        if (event.getEventDate() != null) {
+            SimpleDateFormat sdf = new SimpleDateFormat("MMM dd, yyyy • HH:mm", Locale.getDefault());
+            formattedDate = sdf.format(event.getEventDate().toDate());
+        }
+
+        view.setOnClickListener(v -> {
+            Intent intent = new Intent(getContext(), EventDetailsActivity.class);
+            intent.putExtra("event_name", event.getName());
+            intent.putExtra("event_description", event.getDescription());
+            intent.putExtra("event_location", event.getLocation());
+            intent.putExtra("event_date", formattedDate);
+            intent.putExtra("event_image", event.getImageUrl());
+            getContext().startActivity(intent);
+        });
+
+
         return view;
     }
 }
