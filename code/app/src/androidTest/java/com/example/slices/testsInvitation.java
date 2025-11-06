c63d2e66a1a26aa0632022985a0aa76fb58d62da
roadkillrh <ryanrotter@gmail.com>
2025-11-06 07:47:34 -0700

    Small changes to tests and dependencies
    Updated dependencies for firebase auth and added a test for invitations

--- code/app/src/androidTest/java/com/example/slices/testsInvitation.java
+++ code/app/src/androidTest/java/com/example/slices/testsInvitation.java
@@ -1,4 +1,22 @@
 package com.example.slices;
 
+import com.example.slices.models.Invitation;
+
+import org.junit.Test;
+
 public class testsInvitation {
+
+    @Test
+    public void testCreateInvitation() {
+        // Create a new invitation
+        Invitation invitation = new Invitation("Test Invitation", "Test Message", 1, 2, 3, 4);
+        assert invitation.getTitle().equals("Test Invitation");
+        assert invitation.getBody().equals("Test Message");
+        assert invitation.getId() == 1;
+        assert invitation.getRecipientId() == 2;
+        assert invitation.getSenderId() == 3;
+        assert invitation.getEventId() == 4;
+    }
+
+
 }
--- code/app/src/androidTest/java/com/example/slices/testsInvitationLog.java
+++ code/app/src/androidTest/java/com/example/slices/testsInvitationLog.java
@@ -1,4 +1,5 @@
 package com.example.slices;
 
 public class testsInvitationLog {
+
 }
