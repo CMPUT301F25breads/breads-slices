a1be8a8b272a898c65adc86e71854f784a2e48a4
bakushiin <braderdely@gmail.com>
2025-11-07 12:03:09 -0700

    Create skeleton UI tests

---
+++ code/app/src/androidTest/java/com/example/slices/uitests/user/MenuTest.java
@@ -0,0 +1,40 @@
+package com.example.slices.uitests.user;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import static org.hamcrest.Matchers.allOf;
+
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import com.example.slices.MainActivity;
+import com.example.slices.R;
+
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MenuTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> scenario =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Before
+    public void setup() {
+        onView(allOf(withId(R.id.MenuFragment), isDescendantOfA(withId(R.id.bottom_nav))))
+                .perform(click());
+
+        onView(withId(R.id.profile_edit_button)).check(matches(isDisplayed()));
+    }
+
+}
---
+++ code/app/src/androidTest/java/com/example/slices/uitests/user/MyEventsTest.java
@@ -0,0 +1,43 @@
+package com.example.slices.uitests.user;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+
+import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
+
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import com.example.slices.MainActivity;
+import com.example.slices.R;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MyEventsTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> scenario =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Before
+    public void setup() {
+        onView(allOf(withId(R.id.BrowseFragment), isDescendantOfA(withId(R.id.bottom_nav))))
+                .perform(click());
+        onView(allOf(ViewMatchers.withId(R.id.MyEventsFragment), isDescendantOfA(withId(R.id.bottom_nav))))
+                .perform(click());
+
+        onView(withId(R.id.confirmed_list)).check(matches(isDisplayed()));
+    }
+
+}
