7486d7a720690ef2189dd933590b04a12a748568
Bhupinder Singh <bhupinder.bhangu9@gmail.com>
2025-11-25 14:30:57 -0700

    LocationManager works with helper functions for creating a Location and getting distance

--- code/app/src/main/java/com/example/slices/controllers/LocationManager.java
+++ code/app/src/main/java/com/example/slices/controllers/LocationManager.java
@@ -14,10 +14,20 @@ import com.google.android.gms.location.Priority;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.example.slices.interfaces.LocationCallback;
 
+/**
+ * A class for requesting user location, getting distances between locations, and constructing them
+ * @author Bhupinder Singh
+ */
 public class LocationManager {
 
     public LocationManager() {}
 
+    /**
+     * Returns the users current location
+     * First checks permissions and requests them if they aren't granted
+     * @param context
+     * @param callback
+     */
     public void getUserLocation(Context context, LocationCallback callback) {
         FusedLocationProviderClient fusedLocationProvider = LocationServices.getFusedLocationProviderClient(context);
 
@@ -28,7 +38,6 @@ public class LocationManager {
             callback.onFailure(new Exception("Permissions not granted for location"));
             return;
         }
-
         fusedLocationProvider.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, null)
         .addOnSuccessListener(new OnSuccessListener<Location>() {
             @Override
@@ -40,6 +49,30 @@ public class LocationManager {
                 }
             }
         });
+    }
+
+    /**
+     * Creates a location with the given longitude and latitude.
+     * Just an easier way to construct a location
+     * @param latitude
+     * @param longitude
+     * @return
+     */
+    public Location constructLocation(double latitude, double longitude) {
+        Location newLocation = new Location("Location");
+        newLocation.setLatitude(latitude);
+        newLocation.setLongitude(longitude);
+        return newLocation;
+    }
+
+    /**
+     * Returns the distance between two locations in KM
+     * @param location1
+     * @param location2
+     * @return
+     */
+    public float getDistanceKm(Location location1, Location location2) {
+        return location1.distanceTo(location2)/1000;
     }
 
 }
